02278126a46447762296bce57a97ad9d80ca3805
andreea <bacea.andreea@yahoo.com>
2025-02-27 14:29:24 +0200

    Secure password encoding in the database. User Authentication: Comprehensive login system with role-based access.

--- src/main/java/com/javacorner/medguide/MedGuideApplication.java
+++ src/main/java/com/javacorner/medguide/MedGuideApplication.java
@@ -3,10 +3,18 @@ package com.javacorner.medguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MedGuideApplication{
     public static void main(String[] args) {
         SpringApplication.run(MedGuideApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
